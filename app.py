2b8efe05a7ae8d3d08d5ba650b1e9c1bb3bca657
eylonbachar <eyli.byli@gmail.com>
2023-09-15 01:36:04 +0300

    requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-import pip
-import os
-
-if not os.path.isfile('requirements_installed.txt'):
-    pip.main(['install', '-r', 'requirements.txt'])
-    with open('requirements_installed.txt', 'w') as f:
-        f.write('Requirements installed.')
-
 import streamlit as st
 import arxiv
 import requests
